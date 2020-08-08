a638ed51fe7fe97479304e9859b2e458dfc0778b
Hassan Rafay <hassanrafay95@gmail.com>
2020-08-08 19:20:42 +0500

    PL-20-New: Add Columns

--- aspnet-core/src/PhoneLelo.Project.Core/Authorization/Users/User.cs
+++ aspnet-core/src/PhoneLelo.Project.Core/Authorization/Users/User.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Abp.Authorization.Users;
 using Abp.Extensions;
+using PhoneLelo.Project.Location;
 
 namespace PhoneLelo.Project.Authorization.Users
 {
@@ -10,6 +12,21 @@ namespace PhoneLelo.Project.Authorization.Users
         public virtual string PhoneNumberCode { get; set; }
 
         public const string DefaultPassword = "123qwe";
+
+        public virtual bool IsLocationFilled { get; set; }
+
+        public long? StateId { get; set; }
+        [ForeignKey("StateId")]
+        public State StateFk { get; set; }
+
+
+        public long? CityId { get; set; }
+        [ForeignKey("CityId")]
+        public City CityFk { get; set; }
+
+        public long? NeighbourhoodId { get; set; }
+        [ForeignKey("NeighbourhoodId")]
+        public Neighbourhood NeighbourhoodFk { get; set; }
 
         public static string CreateRandomPassword()
         {
